1cba220b04c6c1110523e61d9a57398a6aa9dd7b
tac <tac472@gmail.com>
2025-03-10 05:27:55 +0900

    コントローラの作成、ビューの作成、CSSの適応

--- twittery/app/controllers/posts_controller.rb
+++ twittery/app/controllers/posts_controller.rb
@@ -1,47 +1,43 @@
-class PostsController < ApplicationController  
-	def index
-	  @posts = Post.all
-	end
-  
-	def show
-	end
-  
-	def new
-	  @post = Post.new
-	end
-  
-	def edit
-	end
-  
-	def create
-	  @post = Post.new(post_params)
-	  if @post.save
-		redirect_to @post, notice: 'Post was successfully created.'
-	  else
-		render :new, status: :unprocessable_entity
-	  end
-	end
-  
-	def update
-	  if @post.update(post_params)
-		redirect_to @post, notice: 'Post was successfully updated.'
-	  else
-		render :edit, status: :unprocessable_entity
-	  end
-	end
-  
-	def destroy
-	  @post.destroy
-	  redirect_to posts_url, notice: 'Post was successfully destroyed.'
-	end
-  
-	private
-  
-	def set_post
-	  @post = Post.find(params[:id])
-	end
-  
-	def post_params
-	  params.require(:post).permit(:title, :content)
-	end
+
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post, notice: 'ツイートしました.'
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post, notice: '更新されました.'
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path, notice: '削除されました'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
+
--- twittery/db/schema.rb
+++ twittery/db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_09_160013) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_09_182143) do
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
